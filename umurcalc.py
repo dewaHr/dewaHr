068a4ab2006af70ee35065b8adddbfb17cc90a64
darkoboy <118077486+dewaahr@users.noreply.github.com>
2022-11-23 16:31:23 +0700

    Add files via upload

---
+++ umurcalc.py
@@ -0,0 +1,8 @@
+import datetime
+tanggal=datetime.date.today()
+tahunini=tanggal.year
+bulanini=tanggal.month
+print("Tanggal Hari ini:",tanggal)
+d,m,y=input("Masukan Tanggal Lahir Anda(ddmmyyyy):").split()
+y=int(y)
+print("Umur Anda:",tahunini-y,"Tahun")
