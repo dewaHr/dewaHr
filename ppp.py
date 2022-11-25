d63121e1896b0b689dd7b9f1beba768f4aedd416
darkoboy <118077486+dewaahr@users.noreply.github.com>
2022-11-25 09:00:32 +0700

    Add files via upload

---
+++ ppp.py
@@ -0,0 +1,52 @@
+# bulan31=(1,3,5,7,8,10,12)
+# bulan30=4,6,9,11
+# bulan28=2
+# masuk=int(input("masukan bulan="))
+# if masuk==bulan31:
+#     print("31 hari")
+# else : print("")
+
+
+# # angka=int(input("masukan"))
+# # if angka/2==0:
+# #     print("genap")
+# # else: print("ganjil")
+
+
+
+# masukanAngka = input("Masukan Angka : ") 
+# if '.' in masukanAngka: 
+#      masukanAngka = float(masukanAngka) 
+#      print("Bilangan Desimal") 
+# else: 
+#      masukanAngka = int(float(masukanAngka)) 
+#      print("Bilanga Bulat")
+# inputName = input("Enter Your Name : ") 
+# if inputName == ("Bond"): 
+#      print("Welcome Agent 007") 
+#  else: 
+#      print("Welcome", inputName)
+
+
+
+# makanan=int(input("harga makanan sebesar="))
+# snack=int(input("harga snack sebesar="))
+# minum=int(input("harga minum sebesar="))
+# uang=int(input("uang yang dibawa="))
+# total=(makanan+snack+minum)
+# utang = total-uang
+# kembalian=uang-total
+# print("yang harus dibayar",total)
+# if uang < total:
+#     print("uang anda kurang, utang=",utang)
+# elif uang==total:
+#     print("uang pas",uang)
+# elif uang>total:
+#     print("kembalian", kembalian)
+
+
+a= int(input("nilai a="))
+b= int(input("nilai b="))
+c= int(input("nilai c="))
+fungsi=b*b+(4*a*c)
+print(fungsi)
